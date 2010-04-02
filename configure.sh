e3b6f3d9d49d669e5e2e92146ae1d5ccbf7117ab
Jalla <jalla@tgbuntu.(none)>
2010-04-02 18:08:30 +0200

    Change configure script to check for libspotify-0.0.3 and download it if needed

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ make clean 2>/dev/null
 rm *.pro 2>/dev/null
 rm Makefile 2>/dev/null
 echo "Generating makefile..."
-qmake -project -o juniper.pro
+qmake -project -norecursive -o juniper.pro
 echo "QT += network" >> juniper.pro
 echo "Generating meta objects..."
 qmake
@@ -12,4 +12,21 @@ echo "Adding libspotify compile flags..."
 # warning. this works, but creates a file named "cat". TODO: correct
 cat make.libflags > cat Makefile > /tmp/juniper.make.temp && mv /tmp/juniper.make.temp Makefile
 sed "s/-pipe -O2 -Wall -W/\$(SPOTFLAGS) -pipe -O2 -Wall -W/" Makefile > tmp.tmp && mv tmp.tmp Makefile
+echo -n "Checking for libspotify..."
+if [ ! -e /usr/lib/libspotify.so ]
+then
+    echo "Not found. Installing..."
+    if [ ! -e libspotify-0.0.3-linux6-i686 ]
+    then
+	wget http://developer.spotify.com/download/libspotify/libspotify-0.0.3-linux6-i686.tar.gz
+	tar xfz libspotify-0.0.3-linux6-i686.tar.gz
+	cd libspotify-0.0.3-linux6-i686 && sudo make install prefix=/usr/
+    fi
+else
+    echo "Found!"
+fi
+if [ "$1" == "all" ]
+then
+    sudo aptitude install libqt4-dev g++ libsndfile-dev
+fi
 echo "All done!"
